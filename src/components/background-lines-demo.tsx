1cff1e2271a099bef6e5a5db7d0870d005087962
Urvil Patel <uvpatel7271@gmail.com>
2026-08-20 21:43:57 +0530

    Added darkmode and proper routing

--- src/components/background-lines-demo.tsx
+++ src/components/background-lines-demo.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { BackgroundLines } from "@/components/ui/background-lines";
+import HoverBorderGradientDemo from "./hover-border-gradient-demo";
+import { ModeToggle } from "./modetoggle";
 
 export default function BackgroundLinesDemo() {
   return (
     <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+     
       <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
         Sanjana Airlines, <br /> Sajana Textiles.
       </h2>
@@ -11,6 +14,7 @@ export default function BackgroundLinesDemo() {
         Get the best advices from our experts, including expert artists,
         painters, marathon enthusiasts and RDX, totally free.
       </p>
+      <HoverBorderGradientDemo />
     </BackgroundLines>
   );
 }
---
+++ src/components/nav-main.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+import { ChevronRightIcon } from "lucide-react"
+import Link from "next/link"
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string
+    url: string
+    icon?: React.ReactNode
+    isActive?: boolean
+    items?: {
+      title: string
+      url: string
+    }[]
+  }[]
+}) {
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Platform</SidebarGroupLabel>
+      <SidebarMenu>
+        {items.map((item) => (
+          <Collapsible
+            key={item.title}
+            defaultOpen={item.isActive}
+            className="group/collapsible"
+            render={<SidebarMenuItem />}
+          >
+            <CollapsibleTrigger
+              render={<SidebarMenuButton tooltip={item.title} />}
+            >
+              {item.icon}
+            <Link href={item.title.toLowerCase()}> <span>{item.title}</span></Link>  
+              <ChevronRightIcon className="ml-auto transition-transform duration-200 group-data-open/collapsible:rotate-90" />
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <SidebarMenuSub>
+                {item.items?.map((subItem) => (
+                  <SidebarMenuSubItem key={subItem.title}>
+                    <SidebarMenuSubButton render={<a href={subItem.url} />}>
+                     <Link href={subItem.url}> <span>{subItem.title} </span></Link>
+                    </SidebarMenuSubButton>
+                  </SidebarMenuSubItem>
+                ))}
+              </SidebarMenuSub>
+            </CollapsibleContent>
+          </Collapsible>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  )
+}
