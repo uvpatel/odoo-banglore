01b53c57df55ebaa3e582ae15dd5293795609c51
Urvil Patel <uvpatel7271@gmail.com>
2026-08-20 21:01:26 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import BackgroundLinesDemo from "@/components/background-lines-demo";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+   <BackgroundLinesDemo />
+   </>
+  );
+}
